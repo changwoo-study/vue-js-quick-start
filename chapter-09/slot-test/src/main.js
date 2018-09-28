3f4384942505e83a80e3ac36d11e50676606d513
changwoo <changwoo@ivynet.co.kr>
2018-09-28 22:22:54 +0900

    Scoped Slot 실습.

--- chapter-09/slot-test/src/main.js
+++ chapter-09/slot-test/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 // import App from './App.vue'
-import App from './AppNamed.vue'
+// import App from './AppNamed.vue'
+import App from './AppScoped.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
