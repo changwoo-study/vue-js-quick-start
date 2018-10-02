6554d61c128aa19ac4efef917ac89377583de15c
changwoo <changwoo@ivynet.co.kr>
2018-10-02 21:27:05 +0900

    11.4 까지 실습한 내용 커밋.

---
+++ chapter-11/countryapp/src/components/RegionButtons.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button class="region" v-for="region in regions"
+                v-bind:key="region"
+                v-bind:class="isSelected(region)"
+                v-on:click="changeRegion({region: region})">
+            {{ region }}
+        </button>
+    </div>
+</template>
+
+<script>
+    import Constant from '../Constant';
+    import {mapGetters, mapMutations} from 'vuex';
+
+    export default {
+        name: 'RegionButtons',
+        computed: mapGetters(['regions', 'currentRegion']),
+        methods: {
+            isSelected(region) {
+                return {selected: region === this.currentRegion}
+            },
+            ...mapMutations([Constant.CHANGE_REGION])
+        }
+    }
+</script>
+
+<style scoped>
+    button.region {
+        text-align: center;
+        width: 80px;
+        margin: 2px;
+        border: solid 1px gray;
+    }
+
+    button.selected {
+        background-color: purple;
+        color: aqua;
+    }
+</style>
