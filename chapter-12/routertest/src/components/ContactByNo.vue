6a07e260442c64ea914545ffa11ceb714fb26600
changwoo <changwoo@ivynet.co.kr>
2018-10-03 22:22:19 +0900

    12.5 까지 실습 코드 커밋.

---
+++ chapter-12/routertest/src/components/ContactByNo.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <hr class="divider">
+        <div>
+            <table class="detail table table-bordered">
+                <tbody>
+                <tr class="active">
+                    <td>일련번호</td>
+                    <td>{{contact.no}}</td>
+                </tr>
+                <tr class="active">
+                    <td>이름</td>
+                    <td>{{contact.name}}</td>
+                </tr>
+                <tr class="active">
+                    <td>전화</td>
+                    <td>{{contact.tel}}</td>
+                </tr>
+                <tr class="active">
+                    <td>주소</td>
+                    <td>{{contact.address}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import contactlist from '../ContactList'
+
+    export default {
+        name: "ContactByNo",
+        data: function () {
+            return {
+                no: 0,
+                contacts: contactlist.contacts
+            }
+        },
+        created: function () {
+            this.no = this.$route.params.no;
+        },
+        watch: {
+            '$route': function (to, from) {
+                this.no = to.params.no;
+            }
+        },
+        computed: {
+            contact: function () {
+                let no = parseInt(this.no),
+                    arr = this.contacts.filter(item => item.no === no);
+                if (arr.length === 1) {
+                    return arr[0];
+                } else {
+                    return {};
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    table.detail {
+        width: 400px;
+    }
+
+    .divider {
+        height: 3px;
+        margin-left: auto;
+        margin-right: auto;
+        background-color: #ff0066;
+        color: #ff0066;
+        border: 0 none;
+    }
+</style>
